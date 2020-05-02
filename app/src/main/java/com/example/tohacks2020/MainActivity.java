7183674f6e08584bd1887403efc92464b6b51c24
Monicalr0 <mmymonicalr@gmail.com>
2020-05-02 18:48:50 -0400

    Added the start button of entering page, create the setting page.

--- app/src/main/java/com/example/tohacks2020/MainActivity.java
+++ app/src/main/java/com/example/tohacks2020/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.tohacks2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * @param view
+     * Start using the app, first turn to the setting page
+     */
+    public void StartUsing(View view){
+        Intent start = new Intent(this, SettingActivity.class);
+        startActivity(start);
+    }
+
 }
