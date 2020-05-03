44222ef363c101a851217a6d46e7959397148126
Monicalr0 <mmymonicalr@gmail.com>
2020-05-02 23:49:30 -0400

    Finished the instruction part of setting.

--- app/src/main/java/com/example/tohacks2020/SettingActivity.java
+++ app/src/main/java/com/example/tohacks2020/SettingActivity.java
@@ -3,6 +3,8 @@ package com.example.tohacks2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -11,6 +13,14 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
     }
+
+    public void ShowInstruction(View view){
+        TextView instruction = findViewById(R.id.instruction);
+        instruction.setText(R.string.Setting_Instruction);
+    }
+
+
+
 
 
 }
