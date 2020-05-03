d84037a4e40b1685ff05cddb58e505bac894ddea
Monicalr0 <mmymonicalr@gmail.com>
2020-05-03 00:51:32 -0400

    Create MonitorActivity, done intent of Setting.

--- app/src/main/java/com/example/tohacks2020/SettingActivity.java
+++ app/src/main/java/com/example/tohacks2020/SettingActivity.java
@@ -2,12 +2,16 @@ package com.example.tohacks2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SettingActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.tohacks2020.SettingActivity.MESSAGE";
+    public static String Default = "14";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,22 @@ public class SettingActivity extends AppCompatActivity {
         instruction.setText(R.string.Setting_Instruction);
     }
 
+    public void StartMonitor(View view){
+        EditText editText = findViewById(R.id.avgFrequency);
+        String answer = editText.getText().toString();
+        Default = answer;
+        Intent intent = new Intent(this, MonitorAcctivity.class);
+        intent.putExtra(EXTRA_MESSAGE, answer);
+        startActivity(intent);
+        finish();
+    }
+
+    public void SkipMonitor(View view){
+        Intent intent = new Intent(this, MonitorAcctivity.class);
+        intent.putExtra(EXTRA_MESSAGE, Default);
+        startActivity(intent);
+        finish();
+    }
 
 
 
