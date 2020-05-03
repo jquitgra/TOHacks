c483ff8ddca1ed00a5ea58bce03c5b1d24a6cb4b
jquitgra <john.qgraham1@gmail.com>
2020-05-03 01:16:12 -0400

    simple camera class implemented from tutorial

--- app/src/main/java/com/example/tohacks2020/MainActivity.java
+++ app/src/main/java/com/example/tohacks2020/MainActivity.java
@@ -4,15 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.View;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.core.Mat;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
     /**
      * @param view
