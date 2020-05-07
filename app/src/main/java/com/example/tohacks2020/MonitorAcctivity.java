b56030c39f9b6f7e65cd7de4656a8187ef41779c
jquitgra <john.qgraham1@gmail.com>
2020-05-06 22:46:20 -0400

    added tfLite implementation for monitorActivity

--- app/src/main/java/com/example/tohacks2020/MonitorAcctivity.java
+++ app/src/main/java/com/example/tohacks2020/MonitorAcctivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import org.tensorflow.lite.Interpreter;
 
 public class MonitorAcctivity extends AppCompatActivity {
 
